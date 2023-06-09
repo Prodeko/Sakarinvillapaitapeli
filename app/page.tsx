20bcdda289465ade697f07f9444c79879872f61d
ccruzkauppila <ccruzkauppila@live.com>
2023-06-10 00:40:59 +0300

    a

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Chat from '../components/Chat'
 export default function Home() {
   return (
     <div className='w-full h-full' style={{backgroundImage: 'url("Background.jpg")', backgroundSize: "cover"}}>
-      <div className="absolute right-6 top-4 text-6xl" style={{ 'font-family': 'Comic Sans MS' }}>SAKARIN VILLAPAITAPELI</div>
+      <div className="absolute right-6 top-4 text-6xl" style={{ 'font-family': 'Comic Sans MS' }}>SAKARI VILLAPAITAPELI</div>
         <div className='grid grid-cols-1 grid-rows-2 md:grid-rows-1 md:grid-cols-[3fr_1fr] h-full w-full'>
             <NanniWrapper/>
             <Chat />

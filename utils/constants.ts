50951094d1798945c9b66afecbcf7e5ccbf1083d
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2023-06-10 01:43:21 +0300

    change win token

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,4 @@
-export const WIN_CONDITION_STRING = 'voitit pelin'
+export const WIN_CONDITION_STRING = 'VOITIT_PELIN_TOKEN'
 export const WINNING_MESSAGE = 'Okei, puen villapaidan. Voitit pelin!'
 export const MAX_CHAT_LENGTH = 20 // 10 messages on each side
 export const FAKE_LAG_AFTER_CHAT_LIMIT_HARD_CAP_MS = 1500

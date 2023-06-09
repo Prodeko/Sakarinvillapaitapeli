830b0cb2c20519528db3aaea27335fbd574a2856
Leevi Jäkälä <jakala.leevi@gmail.com>
2023-06-09 23:32:00 +0300

    hotfixx

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,6 @@ import next from '../public/next.svg'
 import Chat from '../components/Chat'
 
 export default function Home() {
-  const sendPrompt = async () => {
-    const params =  {
-      "model": "gpt-3.5-turbo",
-      "messages": [{"role": "user", "content": "Moi, miten menee?"}]
-    }
-    const apiKey = process.env.OPENAI_API_KEY
-    const res = await fetch("https://api.openai.com/v1/chat/completions", {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${apiKey}`,
-      },
-      method: "POST",
-      body: JSON.stringify(params),
-    })
-    const { messages } = await res.json()
-    console.log(messages)
-  }
   return (
     <div className='w-full h-full flex'>
         <div className='flex flex-row w-full'>

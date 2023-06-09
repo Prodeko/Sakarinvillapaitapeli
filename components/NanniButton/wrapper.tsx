de24fe9ac3fc131b00e9966b0de28c42867cfa15
ccruzkauppila <ccruzkauppila@live.com>
2023-06-10 00:25:03 +0300

    changes

--- components/NanniButton/wrapper.tsx
+++ components/NanniButton/wrapper.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const NanniWrapper = () => {
   return (
-    <div className="w-full h-full bg-cover bg-no-repeat" style={{backgroundImage: 'url("Sakari.png")', backgroundPosition: "bottom"}}>
+    <div className="w-5/6 h-5/6 padd bg-cover bg-no-repeat absolute bottom-0 right-1/4" style={{backgroundImage: 'url("Sakari.png")', backgroundPosition: "bottom"}}>
 
     </div>
   );

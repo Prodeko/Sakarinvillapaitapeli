6f32f7bca1afbd65cc82f98cca1c9d672861c954
Niklas Linnanen <nlinnanen@gmail.com>
2023-06-10 00:53:06 +0300

    add nanni button interactive

--- components/NanniButton/index.tsx
+++ components/NanniButton/index.tsx
@@ -1,9 +1,19 @@
 'use client'
 import { FunctionComponent } from "react";
 
- const NanniButton = () => {
+ const NanniButton = (props) => {
   // TODO: Proper quotes
-  const quotes = ["raineri", "kuvaa tulee"]
+  const quotes = ["raineri", "kuvaa tulee", 
+'"Doge ftw — Elon Musk" — Sakari',
+'"Ok, but it will cost 3 Doge — Elon Musk" — Sakari',
+'"Dojo 4 Doge — Elon Musk" — Sakari',
+'"Do you want Tesla to accept Doge? — Elon Musk" — Sakari',
+'"No highs, no lows, only Doge — Elon Musk" — Sakari',
+'"Tesla merch buyable with Dogecoin — Elon Musk" — Sakari',
+'"I will keep supporting Dogecoin — Elon Musk" — Sakari',
+'"I will pay a million Dogecoin for proof of this mine’s existence! — Elon Musk" — Sakari',
+'"And you can pay with Doge! — Elon Musk" — Sakari',
+'"But u have to pay in Doge — Elon Musk" — Sakari']
   
   const onClick = () => {
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
@@ -12,7 +22,7 @@ import { FunctionComponent } from "react";
   }
   // TODO: Fix bg color
   return (
-    <div onClick={onClick} className="w-5 h-5 bg-white"/>
+    <input type="button" onClick={onClick} value="?" style={props.style}/>
   );
 }
  
--- components/NanniButton/wrapper.tsx
+++ components/NanniButton/wrapper.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import NanniButton from ".";
 
 const NanniWrapper = () => {
   return (
     <div className="w-full h-full bg-contain bg-no-repeat row-span-0 row-start-2 row-end-3 md:row-start-1 md:row-end-2" style={{backgroundImage: 'url("Sakari.png")', backgroundPosition: "bottom"}}>
-
+      <NanniButton style={{position:"absolute", left:"24%", bottom:"16vw"}}/>
+      <NanniButton style={{position:"absolute", left:"49%", bottom:"16vw"}}/>
     </div>
   );
 };

4d951f590295e396c60cf3cc07ad59b830186c55
Niklas Linnanen <nlinnanen@gmail.com>
2023-06-09 23:50:37 +0300

    =add nanni wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,16 @@
 import Image from 'next/image'
+import NanniWrapper from '@/components/NanniButton/wrapper'
 import next from '../public/next.svg'
 import Chat from '../components/Chat'
 
 export default function Home() {
   return (
-    <div className='w-full h-full flex'>
-        <div className='flex flex-row w-full'>
-          <Image
-              className="flex flex-grow"
-              src="/../public/Sakari.png"
-              alt="Sakke"
-              width={600}
-              height={100}
-              priority
-            />
-            <div className='flex flex-grow-0'>
-              <h1>Sakarin villapaitapeli</h1>
-              <Chat />
+    <div className='w-full h-full'>
+        <div className='flex flex-row h-full w-full'>
+            <NanniWrapper />
+            <div>
+            <h1>Sakarin villapaitapeli</h1>
+            <Chat></Chat>
             </div>
         </div>
     </div>
---
+++ components/NanniButton/wrapper.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const NanniWrapper = () => {
+  return (
+    <div className="w-full h-full bg-cover bg-no-repeat" style={{backgroundImage: 'url("Sakari.png")', backgroundPosition: "bottom"}}>
+
+    </div>
+  );
+};
+
+export default NanniWrapper;
